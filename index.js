59cd92f397b8efb4db1b43ab0ee14418c35d3a23
+ <+>
2022-04-05 09:07:34 -0400

    commit tree 349460cc5ed600a17d2aec3213112adb91152a8f

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(someHighlight = '||') {
+    return function (emphatic = 'a hard worker') {
+        return `You are ${someHighlight}${emphatic}${someHighlight}!`
+    }
+}
+
+
+
+
+// function wrapAdjective(someHighlight) {
+//     if (someHighlight === '' || someHighlight ==='*') {
+//         return function emphatic(someArg) {
+//             someArg = 'a hard worker'
+//             return `You are ${someHighlight}${someArg}${someHighlight}!`
+//         }
+//     }
+//     else if (someHighlight === '' || someHighlight ==='||') {
+//         return function emphatic(someArg) {
+//             someArg = 'a dedicated programmer';
+//             return `You are ${someHighlight}${someArg}${someHighlight}!`
+//         }
+//     }
+// }
